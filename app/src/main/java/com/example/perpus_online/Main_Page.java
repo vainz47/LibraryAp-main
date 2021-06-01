4b131e37015632534c6d86c2b8c86ac8aa71812a
Farli Nahrul Javier <55018626+JvrChaki@users.noreply.github.com>
2021-06-01 15:09:54 +0700

    fix bottomBar

--- app/src/main/java/com/example/perpus_online/Main_Page.java
+++ app/src/main/java/com/example/perpus_online/Main_Page.java
@@ -7,12 +7,44 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Main_Page extends AppCompatActivity {
+<<<<<<< Updated upstream
     TextView username, password;
+=======
+    Fragment f = new FragmentHome();
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.books: //id apa iniiiiiiiiiiiiiiiiiiiiii
+                    f = new FragmentHome();
+                    break;
+                case R.id.assignment:
+                    f = new FragmentAssignment();
+                    break;
+                case R.id.profil:
+                    f = new FragmentProfile();
+                    break;
+                default:
+                    f = new FragmentHome();
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
+            return true;
+        }
+    };
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+<<<<<<< Updated upstream
+=======
+        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
+    }
+>>>>>>> Stashed changes
 
         username = findViewById(R.id.getUsername);
         password = findViewById(R.id.getPassword);
